2b8c9436e44cd933edac90bc3588574bc4413d8a
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2022-04-21 12:54:05 +0100

    Add documentation

--- src/Pandorax.Mot.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Pandorax.Mot.DependencyInjection/ServiceCollectionExtensions.cs
@@ -2,8 +2,20 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Pandorax.Mot.DependencyInjection;
 
-internal static class ServiceCollectionExtensions
+/// <summary>
+/// Provides extension methods for setting up MOT api related services in
+/// an <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Registers an <see cref="IMotService"/> in the <see cref="IServiceCollection"/>.
+    /// Use this method when using dependency injection in your application, such as with
+    /// ASP.NET Core.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="apiKey">Your MOT API key.</param>
+    /// <returns>The same service collection so that multiple calls can be chained.</returns>
     public static IServiceCollection AddMot(this IServiceCollection services, string apiKey)
     {
         ArgumentNullException.ThrowIfNull(services);
